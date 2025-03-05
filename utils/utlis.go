0bb4510501cc2dab9ade15bf6f0d94d10c779cad
Marcin Zub <zub@student.agh.edu.pl>
2025-03-05 14:10:22 +0100

    feature: add data validation
    new layer was added to check data integrity for future processing

---
+++ utils/utlis.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"regexp"
+	"strings"
+)
+
+// checks if a string is uppercase
+func IsUppercase(s string) bool {
+	return s == strings.ToUpper(s)
+}
+
+// checks if a string is lowercase
+func IsLowercase(s string) bool {
+	return s == strings.ToLower(s)
+}
+
+// checks if a swift code is valid
+func IsValidSwiftCodeFormat(swiftCode string) bool {
+	swiftRegex := `^[A-Z0-9]{8}(XXX|[A-Z0-9]{3})?$`
+	matched, _ := regexp.MatchString(swiftRegex, swiftCode)
+	return matched
+}
+
+// checks if a string is not empty
+func IsNotEmpty(s string) bool {
+	return s != ""
+}

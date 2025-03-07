72e5ec047290fde6173b7dc69889516b79dd9fb5
Marcin Zub <zub@student.agh.edu.pl>
2025-03-07 13:41:40 +0100

    feat: addfunction to get collection
    function returns desired connection from .env file for later use

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,9 +6,11 @@ import (
 	"os"
 
 	"github.com/MarcinZ20/bankAPI/config"
+	"github.com/MarcinZ20/bankAPI/pkg/handlers/db"
 	"github.com/MarcinZ20/bankAPI/pkg/handlers/spreadsheet"
 	"github.com/MarcinZ20/bankAPI/pkg/models"
 	"github.com/MarcinZ20/bankAPI/pkg/parser"
+	"github.com/MarcinZ20/bankAPI/pkg/transform"
 	"github.com/MarcinZ20/bankAPI/pkg/validation"
 	"github.com/joho/godotenv"
 )
@@ -25,7 +27,11 @@ func main() {
 	}
 	defer client.Disconnect(context.Background())
 
-	// collection := client.Database(os.Getenv("MONGO_DB")).Collection(os.Getenv("MONGO_COLLECTION"))
+	collection, err := db.GetCollection(client)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	mySpreadsheet := models.GoogleSpreadsheet{
 		SpreadsheetId: os.Getenv("SPREADSHEET_ID"),
 	}
@@ -35,21 +41,20 @@ func main() {
 		fmt.Println("Error")
 	}
 
-	var data []models.Bank
-	err = parser.ParseBankData(response, &data)
+	var rawData []models.Bank
+	err = parser.ParseBankData(response, &rawData)
 	if err != nil {
 		fmt.Println("Error")
 	}
 
-	for _, bank := range data {
+	for _, bank := range rawData {
 		validationResult := validation.ValidateBankEntity(bank)
 		if !validationResult.IsValid {
 			fmt.Println(validationResult.Errors)
 		}
 	}
-	fmt.Println("Validation completed")
 
-	// data := transformer.Transform(bankData)
+	transformedData := transform.Transform(&rawData)
 
-	// db.SaveEntities(collection, data)
+	db.SaveEntities(collection, transformedData)
 }

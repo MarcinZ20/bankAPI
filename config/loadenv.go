39f3882b9981fe4b0167e5bf18a981a1c7fa70e8
Marcin Zub <zub@student.agh.edu.pl>
2025-03-07 18:35:51 +0100

    refactor: update config to use one instance of objects
    api-server and db now can be accessed as a single instance from config

--- config/loadenv.go
+++
@@ -1,7 +0,0 @@
-package config
-
-import "os"
-
-func GetMongoUri() string {
-	return os.Getenv("MONGO_URI")
-}
--- pkg/models/headquarter.go
+++ pkg/models/headquarter.go
@@ -8,7 +8,7 @@ type Headquarter struct {
 	CountryISO2Code string   `bson:"countryISO2" json:"countryISO2"`
 	CountryName     string   `bson:"countryName" json:"countryName"`
 	IsHeadquarter   bool     `bson:"isHeadquarter" json:"isHeadquarter"`
-	SwiftCode       string   `bson:"swift_code" json:"swift_code"`
+	SwiftCode       string   `bson:"swiftCode" json:"swiftCode"`
 	Branches        []Branch `bson:"branches" json:"branches"`
 }
 

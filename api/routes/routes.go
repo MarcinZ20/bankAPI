a217baa8eb8463fb4cbaac5715c378a618ef58cb
Marcin Zub <zub@student.agh.edu.pl>
2025-03-07 18:29:41 +0100

    feature: add first api endpoint
    first endpoint returns correct data

---
+++ api/routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/MarcinZ20/bankAPI/api/handlers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func BankRoutes(app *fiber.App) {
+	app.Get("/v1/swift-codes/:swiftCode", handlers.GetSwiftCodesBySwiftCode)
+	app.Get("/v1/swift-codes/country/:countryISO2code", handlers.GetSwiftCodesByCountryCode)
+	app.Post("/v1/swift-codes", handlers.AddNewSwiftCode)
+	app.Delete("/v1/swift-codes/:swiftCode", handlers.DeleteSwiftCode)
+}
